697df017558d68e4b2f3043710e42a5a8ade99bd
TBillTech <thirdheavenward at yahoo>
2019-05-11 20:34:54 +0000

    Switch to modern cabal package.yaml

--- test/Main.hs
+++ test/Main.hs
@@ -8,4 +8,5 @@ prop_revapp :: [Int] -> [Int] -> Bool
 prop_revapp xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
 
 main :: IO ()
-main = quickCheck prop_revapp
+main = do
+    quickCheck prop_revapp
